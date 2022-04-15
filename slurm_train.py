3a936ae697cf85a9c530ad3c8310602384ae24ff
itay1itzhak <itay1itzhak@gmail.com>
2022-04-15 05:56:46 -0700

    add args to slurm script

--- slurm_train.py
+++ slurm_train.py
@@ -3,13 +3,17 @@ from invoke import run
 import itertools
 from datetime import datetime
 import time
+import argparse
 import torch  # verify torch is avilable when lanching jobs
 
 timestamp = datetime.now().strftime("%M_%S")
 
 
-def run_main():
-    home_dir = "/checkpoint/itayitzhak"
+def run_main(args):
+
+    home_dir = args.home_dir
+    python_path = args.python_path
+    code_dir = args.code_dir
     # all_data_types = ["not_permuted", "permuted", '5gram_permuted']
     # all_data_types = ["permuted", '5gram_permuted']
     # all_data_types = ["ahd_0_3_remove_eos"]
@@ -18,47 +22,44 @@ def run_main():
     # all_data_types = ["adspan_2_local_layers_remove_eos"]
     # all_data_types = ["load_all_attn_remove_eos"]
     # all_data_types = ["load_all_attn_nfreeze_remove_eos"]
-
-    all_data_types = [
-        "remove_eos_not_permuted"
-    ]  # make sure remove_eos flag is TRUE in code!
     # all_data_types = ['bos_and_eos_at_the_end']
     # all_data_types = ["append_bos_remove_eos"]
     # all_data_types = ["append_bos"]
     # all_data_types = ["double_eos"]
     # all_data_types = ["ah_dropout"]
+    all_data_types = args.train_types.split(",")
 
-    train_or_eval = "save_attn"  # save_attn to save attention on templates
+    train_or_eval = args.train_eval_or_save_attn
     zero_attn_perfix = ""  # "zero_attn_right_to_left_eos_free_"#"zero_attn_right_to_left_eos_free_"#"zero_attn_"
-
-    # all_data_sizes = ['tiny','small','all']
-    all_data_sizes = ["all"]
-
-    # all_seeds = [1, 2, 3, 4, 5]
-    all_seeds = [4]
-    # all_templates_to_eval = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    all_templates_to_eval = [1]
-    # all_checkpoints = ["best"]
-    # all_checkpoints = [i for i in range(1, 61)]  # + ["best"]
-    # all_checkpoints = [i for i in range(1, 10)]
-    all_checkpoints = [9]
-
-    # all_text_types = ["natural"]  # ['synthetic','semi_natural', 'natural']
-    all_text_types = ["synthetic"]
+    all_data_sizes = args.train_sizes.split(",")  # ['tiny','small','all']
+    all_seeds = args.seeds.split(",")
+    all_templates_to_eval = args.templates.split(",")
+    all_checkpoints = args.checkpoints.split(
+        ","
+    )  # [i for i in range(10, 18)] + ["best"]
+    all_text_types = args.text_types.split(
+        ","
+    )  # ['synthetic','semi_natural', 'natural']
 
     all_save_attn_cond = []
     for text_type in all_text_types:
         for i in all_templates_to_eval:
-            all_save_attn_cond.extend(
-                [
-                    f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s1_s2",
-                    f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s1p_s2",
-                    f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s3_s2",
-                    # f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_np",
-                    # f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_np_prime",
-                    # f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_vp_prime"
-                ]
-            )
+            if "s_conj" in args.conj_types:
+                all_save_attn_cond.extend(
+                    [
+                        f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s1_s2",
+                        f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s1p_s2",
+                        f"compositional_mt/systematicity/s_conj/{text_type}/systematicity_{text_type}_{i}_s3_s2",
+                    ]
+                )
+            if "s_np_vp" in args.conj_types:
+                all_save_attn_cond.extend(
+                    [
+                        f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_np",
+                        f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_np_prime",
+                        f"compositional_mt/systematicity/s_np_vp/{text_type}/systematicity_{text_type}_{i}_vp_prime",
+                    ]
+                )
 
     if train_or_eval != "save_attn":
         all_save_attn_cond = [None]
@@ -77,7 +78,10 @@ def run_main():
         )
     ):
         configuration = get_configuration(
+            args,
             home_dir,
+            python_path,
+            code_dir,
             data_type,
             data_size,
             seed,
@@ -93,7 +97,10 @@ def run_main():
 
 ##############################################################
 def get_configuration(
+    args,
     home_dir,
+    python_path,
+    code_dir,
     data_type,
     data_size,
     seed,
@@ -105,14 +112,14 @@ def get_configuration(
 ):
     conf = dict()
     conf["home_dir"] = home_dir
+    conf["python_path"] = python_path
+    conf["code_dir"] = code_dir
     conf["checkpoints_dir"] = f"{home_dir}/trained_models/opus/{data_size}/"
     conf["seed"] = seed
     conf["data_size"] = data_size
     conf["train_or_eval"] = train_or_eval
     conf["checkpoint"] = checkpoint
-    conf["save_dir"] = (
-        data_type + f"_seed_{seed}"
-    )  #'5gram_permuted'#'permuted'# 'not_permuted'
+    conf["save_dir"] = data_type + f"_seed_{seed}"
 
     conf["max-tokens"] = "3584"
     conf["patience"] = "10"
@@ -162,7 +169,10 @@ def get_configuration(
         )
         conf["beam"] = "5"
     elif train_or_eval == "train":
-        conf["epoch-checkpoints"] = ""  # "--no-epoch-checkpoints"  # ""
+        if args.save_epoch_checkpoints:
+            conf["epoch-checkpoints"] = ""
+        else:
+            conf["epoch-checkpoints"] = "--no-epoch-checkpoints"
 
     if train_or_eval == "eval" or train_or_eval == "save_attn":  # eval on flores
         conf["nodes"] = "1"
@@ -237,7 +247,7 @@ def translation_get_script_file_text(conf):
         script_file_text = (
             "#!/bin/bash\n\n"
             + "module load anaconda3/5.0.1\nmodule load cudnn/v8.0.3.33-cuda.11.0\nmodule load cuda/11.0\nmodule load openmpi/4.1.0/cuda.11.0-gcc.9.3.0\nsource activate fairseq-20210318\n"
-            + f"/private/home/itayitzhak/.conda/envs/fairseq-20210318/bin/python /private/home/itayitzhak/fairseq/train.py {conf['data_path']} \\\n"
+            + f"{conf['python_path']} {conf['code_dir']}/train.py {conf['data_path']} \\\n"
             + "  --arch transformer_wmt_en_de --share-all-embeddings \\\n"
             + "  --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \\\n"
             + "  --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \\\n"
@@ -259,7 +269,7 @@ def translation_get_script_file_text(conf):
     elif conf["train_or_eval"] == "eval":
         script_file_text = (
             "#!/usr/bin/env bash\n"
-            + "  /private/home/itayitzhak/.conda/envs/fairseq-20210318/bin/python /private/home/itayitzhak/fairseq/fairseq_cli/generate.py \\\n"
+            + f"  {conf['python_path']} {conf['code_dir']}/fairseq_cli/generate.py \\\n"
             + conf["data_path"]
             + " \\\n"
             + "  --path "
@@ -271,7 +281,7 @@ def translation_get_script_file_text(conf):
     elif conf["train_or_eval"] == "save_attn":
         script_file_text = (
             "#!/usr/bin/env bash\n"
-            + "  /private/home/itayitzhak/.conda/envs/fairseq-20210318/bin/python /private/home/itayitzhak/fairseq/fairseq_cli/generate.py \\\n"
+            + f"  {conf['python_path']} {conf['code_dir']}/fairseq_cli/generate.py \\\n"
             + conf["data_path"]
             + " \\\n"
             + "  --path "
@@ -339,7 +349,75 @@ def create_files_and_run(conf):
     print(conf["train_or_eval"])
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--home_dir", type=str, default="/checkpoint/itayitzhak", help=""
+    )
+    parser.add_argument(
+        "--python_path",
+        type=str,
+        default="/private/home/itayitzhak/.conda/envs/fairseq-20210318/bin/python",
+        help="",
+    )
+    parser.add_argument(
+        "--code_dir", type=str, default="/private/home/itayitzhak/globality", help=""
+    )
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default="/checkpoint/itayitzhak/dieuwkehupkes/",
+        help="",
+    )
+
+    parser.add_argument(
+        "--train_types",
+        type=str,
+        default="remove_eos_not_permuted",
+        help="Baselines are remove_eos_not_permuted or not_permuted",
+    )
+    parser.add_argument(
+        "--train_eval_or_save_attn",
+        type=str,
+        default="eval",
+        help="train eval or save_attn",
+    )
+
+    parser.add_argument("--train_sizes", type=str, default="all")
+    parser.add_argument(
+        "--seeds", type=str, default="1,2,3,4,5", help="train eval or save_attn"
+    )
+    parser.add_argument(
+        "--checkpoints",
+        type=str,
+        default="best",
+        help="use in eval or save_attn: best or numbers",
+    )
+    parser.add_argument(
+        "--templates",
+        type=str,
+        default="1,2,3,4,5,6,7,8,9,10",
+        help="use in eval or save_attn: 1 to 10",
+    )
+    parser.add_argument(
+        "--text_types",
+        type=str,
+        default="synthetic,semi_natural,natural",
+        help="use in eval or save_attn: 1 to 10",
+    )
+    parser.add_argument("--conj_types", type=str, default="s_conj,s_np_vp")
+
+    parser.add_argument("--zero_attn_perfix", type=str, default="")
+    parser.add_argument("--save_epoch_checkpoints", type=bool, default=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == "__main__":
-    run_main()
+    args = get_args()
+    run_main(args)
     # print("Slurm not activated, uncomment in main please")
 
